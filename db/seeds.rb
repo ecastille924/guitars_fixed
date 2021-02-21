73f644ad57a8c372685dec3f4675947bafe8fb8f
Erick Castille <ecastille924@gmail.com>
2021-02-21 13:31:10 -0500

    adding some seed data for reference while building app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+RepairBill.create([{brand: "Fender", model: "Telecaster", year:"1987", style: "6-string", electric: true, acoustic: false}])
+RepairBill.create([{brand: "Paul Reed Smith", model: "Dragon 2", year:"1993", style: "6-string", electric: true, acoustic: false}])
+RepairBill.create([{brand: "Yamaha", model: "APX 600", year:"2018", style: "6-string", electric: true, acoustic: true}])
+RepairBill.create([{brand: "Martin", model: "D-15M", year:"2008", style: "6-string", electric: false, acoustic: true}])
+RepairBill.create([{brand: "Fender", model: "Precision Bass", year:"1994", style: "4-string", electric: true, acoustic: false}])
