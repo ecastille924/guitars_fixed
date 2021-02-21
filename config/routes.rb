23451321ed7bac0e095b2ffad32a2c14054744af
Erick Castille <ecastille924@gmail.com>
2021-02-21 12:58:19 -0500

    adding show and index routes for repair bills

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'repair_bills', to: 'repair_bills#index', as: 'repair_bills'
+  get 'repair_bills/:id', to: 'repair_bills#show', as: 'repair_bill'
 end
+
