3a02b0664e3521bc93ef176ce990895d6943d455
Erick Castille <ecastille924@gmail.com>
2021-02-21 16:26:19 -0500

    adding CRUD methods to repair_bill controller

--- app/controllers/repair_bills_controller.rb
+++ app/controllers/repair_bills_controller.rb
@@ -10,6 +10,38 @@ layout false, :except => :show
         @repair_bill = RepairBill.find(params[:id])
     end
 
-    
+    def new 
+        @repair_bill = RepairBill.new
+    end
 
+    def edit 
+        @repair_bill = RepairBill.find(params[:id])
+    end
+
+    def update 
+        repair_bill = RepairBill.find(params[:id])
+        repair_bill.update(repair_bill_params)
+        redirect_to repair_bill_path(repair_bill)
+    end
+
+    def destroy 
+        repair_bill = RepairBill.find(params[:id])
+        repair_bill.destroy
+        redirect_to repair_bill_path
+    end
+
+    def create 
+        @repair_bill = RepairBill.new(repair_bill_params) 
+        if @repair_bill.save 
+            redirect_to repair_bill_path 
+        else
+            render :new
+        end
+    end
+end
+
+private 
+
+def repair_bill_params 
+    params.require(:repair_bill).permit(:brand, :model, :year, :style, :acoustic, :electric)
 end
