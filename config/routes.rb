36ac29456d631d511442c76a7b1c4a6b3ffb7546
Erick Castille <ecastille924@gmail.com>
2021-02-24 19:59:26 -0500

    writing route for omniauth and code for error display in repair bill index view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   resources :repair_bills
+
+  get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
   
   #get 'repair_bills', to: 'repair_bills#index', as: 'repair_bills'
   #get 'repair_bills/:id', to: 'repair_bills#show', as: 'repair_bill'
