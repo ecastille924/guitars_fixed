0443c21b241bc1e6ef21563b57177eb1f2aedcfd
Erick Castille <ecastille924@gmail.com>
2021-02-24 20:15:30 -0500

    moving button for login to the new repair bills page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     def self.create_from_omniauth(auth)
-        User.find_or_create_by(uid: auth[‘uid’], provider: auth[‘provider”]) do |u|
+        User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
             u.username = auth['info']['name']
             u.email = auth['info']['email']
             u.password = SecureRandom.hex(16)    
