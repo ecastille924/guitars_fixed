3922d080894ecb4e704fd4e15c43ded521a42944
Erick Castille <ecastille924@gmail.com>
2021-02-24 19:52:16 -0500

    adding logic to user.rb for login with google/omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    def self.create_from_omniauth(auth)
+        User.find_or_create_by(uid: auth[‘uid’], provider: auth[‘provider”]) do |u|
+            u.username = auth['info']['name']
+            u.email = auth['info']['email']
+            u.password = SecureRandom.hex(16)    
+        end
+    end
 end
