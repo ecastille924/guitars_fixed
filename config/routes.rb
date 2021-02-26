3f27f177b5831a29cc06da6878d5259f24b77db0
Erick Castille <ecastille924@gmail.com>
2021-02-26 18:57:14 -0500

    adding users controller and new user form

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :technicians, only: [:new, :create, :show, :index]
   resources :repair_bills
+  
   get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
+
+get '/signup', to: 'users#new'
+post '/signup', to: 'users#create'
+get '/user/:id', to: 'users#show', as: 'user'
   
   #get 'repair_bills', to: 'repair_bills#index', as: 'repair_bills'
   #get 'repair_bills/:id', to: 'repair_bills#show', as: 'repair_bill'
