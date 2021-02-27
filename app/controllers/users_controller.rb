ff32501f8384bb838a508562cf8999c5e71ed4b8
Erick Castille <ecastille924@gmail.com>
2021-02-26 19:20:08 -0500

    writing out create action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,16 @@ class UsersController < ApplicationController
     end
 
     def create 
+        @user = User.new(user_params)
+        if @user.save 
+          redirect_to '/repair_bills'
+        else 
+          render :new 
+        end
     end
+end
+
+private 
+def user_params 
+    params.require(:user).permit(:username, :email, :password_digest)
 end
