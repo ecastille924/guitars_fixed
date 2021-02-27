3e64332a619618f52f75be59aafaf146b157adc5
Erick Castille <ecastille924@gmail.com>
2021-02-26 19:13:32 -0500

    changing users routes to use resources call

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :technicians, only: [:new, :create, :show, :index]
   resources :repair_bills
-  
+  resources :users 
+
   get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
 
-get '/signup', to: 'users#new'
-post '/signup', to: 'users#create'
-get '/user/:id', to: 'users#show', as: 'user'
+#get '/signup', to: 'users#new'
+#post '/signup', to: 'users#create'
+#get '/user/:id', to: 'users#show', as: 'user'
   
   #get 'repair_bills', to: 'repair_bills#index', as: 'repair_bills'
   #get 'repair_bills/:id', to: 'repair_bills#show', as: 'repair_bill'
