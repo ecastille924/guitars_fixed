e3bdc7d6888927828a24bf4452da19adc2c0701a
Erick Castille <ecastille924@gmail.com>
2021-02-28 16:28:36 -0500

    adding some text to the home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
     has_secure_password
     has_many :repair_bills 
     has_many :technicians, through: :repair_bills
-    
+    validates :username, :email, uniqueness: true
+    validates :username, :email, presence: true
+
     def self.create_from_omniauth(auth)
         User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
             u.username = auth['info']['name']
