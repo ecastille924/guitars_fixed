77b0ed43e3db075fe0fa052fe7912cb98e229b24
Erick Castille <ecastille924@gmail.com>
2021-02-28 22:31:15 -0500

    removing some unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,4 @@ class ApplicationController < ActionController::Base
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
-    def logged_in?
-        !!session[:user_id]
-    end
 end
