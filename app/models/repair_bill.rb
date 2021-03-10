9f0c1411ee7b490ea8f744ac35243fcf8385f17c
Erick Castille <ecastille924@gmail.com>
2021-03-10 12:04:47 -0500

    adding 'current technician' to repair bills show page

--- app/models/repair_bill.rb
+++ app/models/repair_bill.rb
@@ -2,7 +2,7 @@ class RepairBill < ApplicationRecord
     belongs_to :technician
     belongs_to :user
     validates :brand, :model, :style, :reason_for_repair, presence:true
-    #accepts_nested_attributes_for :technician
+    
 
     def year_brand_model 
         "#{self.year} #{self.brand} #{self.model}"
@@ -11,4 +11,5 @@ class RepairBill < ApplicationRecord
     def technician_attributes=(attr)
         self.technician = Technician.find_by(name: attr[:name])
     end
+
 end
