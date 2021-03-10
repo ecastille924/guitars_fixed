6cd95108f0c799a88a20751bb7ee521f2f2f7cb0
Erick Castille <ecastille924@gmail.com>
2021-03-10 14:04:50 -0500

    removing unused code from routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,27 +10,16 @@ Rails.application.routes.draw do
 
   resources :reviews
   
-   
-  
-  
-  
   resources :repair_bills
   resources :users 
   post '/repair_bills/:id/edit', to: 'repair_bills#edit'
   
-
-  #resources :sessions
   get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-#post '/signup', to: 'users#create'
-#get '/user/:id', to: 'users#show', as: 'user'
-  
-  #get 'repair_bills', to: 'repair_bills#index', as: 'repair_bills'
-  #get 'repair_bills/:id', to: 'repair_bills#show', as: 'repair_bill'
 
 end
 
