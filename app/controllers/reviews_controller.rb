e1ad3492a746e47f7f008ef91d57fca6b3ebe6d3
Erick Castille <ecastille924@gmail.com>
2021-03-10 14:39:01 -0500

    removing unused code from reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,7 @@
 class ReviewsController < ApplicationController
 
   def index 
-    #if params[:technician_id]
-      #@reviews = Technician.find(params[:technician_id])
-    #else
       @reviews = Review.all
-    #end
   end
 
   def show 
@@ -16,13 +12,10 @@ class ReviewsController < ApplicationController
   def new 
       @technician = Technician.find_by(id: params[:technician_id])
       @review = Review.new(technician_id: params[:technician_id])
-      #@repair_bill.build_technician
   end
 
   def create
-    #@technician = Technician.find(params[:technician_id])
     @review = Review.new(review_params.merge(user_id: current_user.id))
-    #@review = @technician.review.build(params[:review_params])
     
     if @review.save
       redirect_to technicians_path
